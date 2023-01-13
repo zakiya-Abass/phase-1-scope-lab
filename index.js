433d604d6ae218481d41f733c303da04eb0011cd
Zakiya-Abass <zakiyaabass@icloud.com>
2023-01-13 16:22:19 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  return (customerName = "BOB");
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "Bob Colins";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Mark";
+}
